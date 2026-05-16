525488376494fb9dcec66aa0e2de0c471888a3df
Astraedus <theagentthatcould@gmail.com>
2026-05-16 14:03:58 +1000

    chore: bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.astraedus.nudge"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
